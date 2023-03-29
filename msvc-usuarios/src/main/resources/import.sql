dd979512a18bbc1fcb12ddd0984de48f6d6e3c97
damian <adamodamian6@gmail.com>
2023-03-29 16:13:16 -0300

    antes de docker

--- msvc-usuarios/src/main/resources/import.sql
+++ msvc-usuarios/src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO `usuarios` (username,password,nombre,apellido,email,enabled) VALUES('damian','$2a$10$0zPDGpery7qFyrZbfcnHGONn.6L0M/Y7m3og3.FX/ImYd9eHIyWfO','damian','adamo','damiang66@gmail.com',1);
-INSERT INTO `usuarios` (username,password,nombre,apellido,email,enabled)  VALUES('admin','$2a$10$igOu2LKFqFDIAvjZg6YZZezMpYj6WlZmeA6BLsqq3rZZKEm4WngPS','camila','adamo','camila@gmail.com',1);
+INSERT INTO usuarios (username,password,nombre,apellido,email,enabled) VALUES('damian','$2a$10$0zPDGpery7qFyrZbfcnHGONn.6L0M/Y7m3og3.FX/ImYd9eHIyWfO','damian','adamo','damiang66@gmail.com',true);
+INSERT INTO usuarios (username,password,nombre,apellido,email,enabled)  VALUES('admin','$2a$10$igOu2LKFqFDIAvjZg6YZZezMpYj6WlZmeA6BLsqq3rZZKEm4WngPS','camila','adamo','camila@gmail.com',true);
 
-INSERT INTO `roles` (nombre) VALUES('ROLE_USER');
-INSERT INTO `roles` (nombre) VALUES('ROLE_ADMIN');
-INSERT INTO `usuarios_roles` (usuario_id,role_id) VALUES(1,1);
-INSERT INTO `usuarios_roles` (usuario_id,role_id) VALUES(2,2);
-INSERT INTO `usuarios_roles` (usuario_id,role_id) VALUES(2,1);
+INSERT INTO roles (nombre) VALUES('ROLE_USER');
+INSERT INTO roles (nombre) VALUES('ROLE_ADMIN');
+INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(1,1);
+INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(2,2);
+INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(2,1);
